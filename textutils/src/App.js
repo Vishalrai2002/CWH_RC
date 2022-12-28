3b90e45932533501d55683839514e1dea08d18bf
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2022-12-28 07:43:39 +0530

    video no 5

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -5,18 +5,29 @@ let name="Vishal";
 function App() {
   return (
   <>
-  <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact Us</li>
-    <li>Login/SignUp</li>
-  </nav>
-  <div className="container">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur quibusdam mollitia, totam eligendi illo nesciunt sequi officia quisquam eius aliquam delectus, illum reprehenderit quos tenetur dolor ipsum error similique corrupti perspiciatis accusamus. Ducimus, natus.</p>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/'">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/'">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/'">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
-  <h1>Hello my name is {name}</h1>
+</nav>
   </>
-  
   );
 }
 
