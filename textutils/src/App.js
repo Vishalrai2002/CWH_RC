f70f3ba8df67f389b39e12571882e622217108a6
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2022-12-30 08:26:01 +0530

    video 11

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import about from './components/About';
-import About from './components/About';
+import TextForm from './components/TextForm';
+// import About from './components/About';
 
 
 
@@ -14,7 +13,7 @@ function App() {
 
   <Navbar title="TextUtils"abouttext="About"/>
   <div className="container my-3">
-    <About/>
+    <TextForm/>
   </div>
 
   </>
