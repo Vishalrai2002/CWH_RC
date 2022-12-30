dc9f57ba54c0d36bdf9c8f172f4e65c065deb8df
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2022-12-30 09:14:31 +0530

    Video 12

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,18 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react'
 // import About from './components/About';
 
 
 
-let name="Vishal";
+// let name="Vishal";
+
+
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark-mode is enabled or not
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
   return (
   <>
 
-  <Navbar title="TextUtils"abouttext="About"/>
+  <Navbar title="TextUtils"abouttext="About"  mode={mode} toggleMode={toggleMode}/>
   <div className="container my-3">
-    <TextForm/>
+    <TextForm heading="Enter the Text to Analyze below" mode={mode} />
   </div>
 
   </>
