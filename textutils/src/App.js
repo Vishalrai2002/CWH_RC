0e7bdb2934a624eadab39e88df8be79c1f28180e
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-01-03 06:35:48 +0530

    All set

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,22 +1,20 @@
+import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
-import React, { useState } from 'react';
+import React,{useState} from 'react'
 import Alert from './components/Alert';
-import {Route} from 'react-router-dom/Route';
+// import About from './components/About';
 
 
-import {
-  BrowserRouter,
-  Routes,
-  Link
-} from "react-router-dom";
+
+// let name="Vishal";
 
 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
-  const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light'); // whether dark-mode is enabled or not
+  const [alert, setAlert] = useState(null); // for setting the alert 
+
 
   const showAlert = (message, type)=>{
       setAlert({
@@ -28,27 +26,29 @@ function App() {
       }, 1500);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode=()=>{
+    if(mode==='light'){
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      showAlert("Dark mode has been enabled", "success");
+      document.body.style.backgroundColor='#042743';
+      showAlert(" Dark Mode has been Enabled","success")
     }
     else{
       setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light mode has been enabled", "success");
+      document.body.style.backgroundColor='white';
+      showAlert(" Light Mode has been Enabled","success")
     }
   }
+
   return (
-    <>
-    <BrowserRouter>
-    <Routes>
-        <Route path="/about" element={<About/>} />
-        <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />} />
-      </Routes>
-    </BrowserRouter>
-    </> 
+  <>
+
+  <Navbar title="Text-Modifier"abouttext="About"  mode={mode} toggleMode={toggleMode}/>
+  <Alert alert={alert} />
+  <div className="container my-3">
+    <TextForm showAlert={showAlert} heading="Enter the Text to Analyze below" mode={mode} />
+  </div>
+
+  </>
   );
 }
 
